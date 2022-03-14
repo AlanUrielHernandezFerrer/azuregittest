4b61e321c0dcc9806f2c2579dfb74a006f1c758b
AlanUrielHernandezFerrer <alan.ferre3212@gmail.com>
2022-03-14 10:53:31 -0600

    Initial Commit

---
+++ azure/azureApp.swift
@@ -0,0 +1,17 @@
+//
+//  azureApp.swift
+//  azure
+//
+//  Created by alan uriel on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct azureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
